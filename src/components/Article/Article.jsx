b4cb9bfba16172e138fc14fc137d93194d35b741
Stanislav Kishechko <skishechko@gmail.com>
2026-02-25 20:07:00 +0200

    Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -22,20 +22,21 @@ function Article() {
       <p className="article__paragraph">
         In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae,
         fermentum ac quam. Aliquam pretium tristique nibh quis iaculis. In et
-        cursus ex, eu aliquet ex. Proin facilisis lacus sit amet sapien ultrices,
-        ut vehicula arcu lobortis. Vivamus mollis ipsum ut hendrerit molestie.
-        Morbi lacinia, sapien eu dictum dignissim, tellus tortor congue magna,
-        sit amet bibendum libero nisi id massa.
+        cursus ex, eu aliquet ex. Proin facilisis lacus sit amet sapien
+        ultrices, ut vehicula arcu lobortis. Vivamus mollis ipsum ut hendrerit
+        molestie. Morbi lacinia, sapien eu dictum dignissim, tellus tortor
+        congue magna, sit amet bibendum libero nisi id massa.
       </p>
       <p className="article__paragraph">
         Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor. Cras
         congue, massa nec sagittis mollis, dui felis ultrices magna, tincidunt
-        finibus lorem quam in sem. Morbi odio turpis, pulvinar sit amet vulputate
-        quis, ultricies eu libero. Donec ac maximus neque, nec maximus nibh.
-        Morbi rhoncus convallis urna, accumsan porta lorem hendrerit in. Cras
-        eget nisl dui. Morbi faucibus nisi eget ipsum semper vulputate. Mauris
-        nec tincidunt lectus. Aenean ac mi consequat velit dignissim consectetur.
-        Fusce placerat ac ipsum ac eleifend. Aenean quis faucibus ex.
+        finibus lorem quam in sem. Morbi odio turpis, pulvinar sit amet
+        vulputate quis, ultricies eu libero. Donec ac maximus neque, nec maximus
+        nibh. Morbi rhoncus convallis urna, accumsan porta lorem hendrerit in.
+        Cras eget nisl dui. Morbi faucibus nisi eget ipsum semper vulputate.
+        Mauris nec tincidunt lectus. Aenean ac mi consequat velit dignissim
+        consectetur. Fusce placerat ac ipsum ac eleifend. Aenean quis faucibus
+        ex.
       </p>
       <p className="article__paragraph">
         Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
@@ -43,8 +44,8 @@ function Article() {
         Mauris egestas magna ut laoreet pretium. Sed ultrices suscipit
         vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
         Fusce id sapien eros. Vivamus viverra ultricies gravida. Nam urna nibh,
-        blandit a vulputate at, vehicula non nulla. Aenean ut nulla leo. Praesent
-        in ullamcorper est.
+        blandit a vulputate at, vehicula non nulla. Aenean ut nulla leo.
+        Praesent in ullamcorper est.
       </p>
       <p className="article__paragraph">
         Pellentesque habitant morbi tristique senectus et netus et malesuada
